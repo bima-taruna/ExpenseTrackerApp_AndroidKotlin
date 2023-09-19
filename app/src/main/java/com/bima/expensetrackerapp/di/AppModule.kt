fa2474e3de2cd8fe54626ab0b490976c8102126e
Mhd Bima Taruna Cipta <bimataruna21@gmail.com>
2023-09-19 10:13:02 +0700

    implementing login screen

---
+++ app/src/main/java/com/bima/expensetrackerapp/di/AppModule.kt
@@ -0,0 +1,60 @@
+package com.bima.expensetrackerapp.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.github.jan.supabase.SupabaseClient
+import io.github.jan.supabase.annotations.SupabaseExperimental
+import io.github.jan.supabase.createSupabaseClient
+import javax.inject.Singleton
+import com.bima.expensetrackerapp.BuildConfig
+import io.github.jan.supabase.gotrue.FlowType
+import io.github.jan.supabase.gotrue.GoTrue
+import io.github.jan.supabase.gotrue.gotrue
+import io.github.jan.supabase.postgrest.Postgrest
+import io.github.jan.supabase.postgrest.postgrest
+import io.github.jan.supabase.storage.Storage
+import io.github.jan.supabase.storage.storage
+
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+    @OptIn(SupabaseExperimental::class)
+    @Provides
+    @Singleton
+    fun provideSupabaseClient() : SupabaseClient {
+        return createSupabaseClient(
+            supabaseUrl = BuildConfig.SUPABASE_URL,
+            supabaseKey = BuildConfig.API_KEY
+        ) {
+            install(Postgrest)
+            install(GoTrue) {
+                flowType = FlowType.PKCE
+                scheme = "app"
+                host = "supabase.com"
+            }
+            install(Storage)
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun provideSupabaseDatabase(client: SupabaseClient): Postgrest {
+        return client.postgrest
+    }
+
+    @Provides
+    @Singleton
+    fun provideSupabaseGoTrue(client: SupabaseClient): GoTrue {
+        return client.gotrue
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideSupabaseStorage(client: SupabaseClient): Storage {
+        return client.storage
+    }
+}
