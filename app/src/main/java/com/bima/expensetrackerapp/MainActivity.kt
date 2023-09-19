ed3e0829288088b25185d8731e6d37e879bddd1d
Mhd Bima Taruna Cipta <bimataruna21@gmail.com>
2023-09-19 09:24:28 +0700

    add hilt dependencies

--- app/src/main/java/com/bima/expensetrackerapp/MainActivity.kt
+++ app/src/main/java/com/bima/expensetrackerapp/MainActivity.kt
@@ -10,8 +10,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.bima.expensetrackerapp.presentation.ExpenseTrackerApp
 import com.bima.expensetrackerapp.ui.theme.ExpenseTrackerAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +25,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                   ExpenseTrackerApp()
                 }
             }
         }
---
+++ app/src/main/java/com/bima/expensetrackerapp/presentation/ExpenseTrackerApp.kt
@@ -0,0 +1,9 @@
+package com.bima.expensetrackerapp.presentation
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ExpenseTrackerApp() {
+    Text(text = "HALLLOOOOOOO")
+}
