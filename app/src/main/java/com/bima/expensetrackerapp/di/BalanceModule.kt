cff944814791d4bd1bf7da17fc32ce649927ed1d
Mhd Bima Taruna Cipta <bimataruna21@gmail.com>
2023-10-28 20:10:09 +0700

    create function provide balance use cases on BalanceModule

--- app/src/main/java/com/bima/expensetrackerapp/di/BalanceModule.kt
+++ app/src/main/java/com/bima/expensetrackerapp/di/BalanceModule.kt
@@ -2,6 +2,11 @@ package com.bima.expensetrackerapp.di
 
 import com.bima.expensetrackerapp.data.repositoryImpl.BalanceRepositoryImpl
 import com.bima.expensetrackerapp.domain.repository.BalanceRepository
+import com.bima.expensetrackerapp.domain.repository.UserProfileRepository
+import com.bima.expensetrackerapp.domain.use_case.balance.BalanceUseCases
+import com.bima.expensetrackerapp.domain.use_case.balance.GetBalanceUseCase
+import com.bima.expensetrackerapp.domain.use_case.user.GetProfileUseCase
+import com.bima.expensetrackerapp.domain.use_case.user.UserUseCases
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,5 +21,13 @@ object BalanceModule {
     @Singleton
     fun provideBalanceRepository(postgrest: Postgrest): BalanceRepository {
         return BalanceRepositoryImpl(postgrest)
+    }
+
+    @Singleton
+    @Provides
+    fun provideBalanceUseCases(repository: BalanceRepository): BalanceUseCases{
+        return BalanceUseCases(
+            getBalance = GetBalanceUseCase(repository)
+        )
     }
 }
