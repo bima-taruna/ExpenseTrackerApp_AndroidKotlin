9a60794c6284bc36afe0af047cf5161f9826a174
Mhd Bima Taruna Cipta <bimataruna21@gmail.com>
2023-10-29 14:59:35 +0700

    display income and expense to homescreen

--- app/src/main/java/com/bima/expensetrackerapp/presentation/HomeScreen.kt
+++ app/src/main/java/com/bima/expensetrackerapp/presentation/HomeScreen.kt
@@ -1,8 +1,10 @@
 package com.bima.expensetrackerapp.presentation
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -21,11 +23,13 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import com.bima.expensetrackerapp.presentation.navigation.Screen
 import com.bima.expensetrackerapp.viewmodel.AuthViewModel
@@ -42,9 +46,9 @@ fun HomeScreen(
     balanceViewModel: BalanceViewModel = hiltViewModel(),
 ) {
     val coroutineScope = rememberCoroutineScope()
-    val sessionState by authViewModel.session.collectAsState()
-    val userState by userViewModel.userState.collectAsState()
-    val balanceState by balanceViewModel.balanceState.collectAsState()
+    val sessionState by authViewModel.session.collectAsStateWithLifecycle()
+    val userState by userViewModel.userState.collectAsStateWithLifecycle()
+    val balanceState by balanceViewModel.balanceState.collectAsStateWithLifecycle()
     LaunchedEffect(sessionState?.user) {
         if (sessionState == null) {
             navController.navigate(Screen.Title.route) {
@@ -59,7 +63,7 @@ fun HomeScreen(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         ConstraintLayout {
-            val (container,totalBalance, space) = createRefs()
+            val (container, totalBalance, space) = createRefs()
             Box(
                 modifier = modifier
                     .fillMaxWidth()
@@ -95,7 +99,7 @@ fun HomeScreen(
                 ),
                 modifier = modifier
                     .fillMaxWidth(0.90f)
-                    .fillMaxHeight(0.25f)
+                    .fillMaxHeight(0.30f)
                     .constrainAs(totalBalance) {
                         top.linkTo(space.bottom, margin = 60.dp)
                         start.linkTo(container.start)
@@ -123,6 +127,44 @@ fun HomeScreen(
                     textAlign = TextAlign.Center,
                     fontWeight = FontWeight.W600
                 )
+                Row(
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight()
+                        .padding(horizontal = 24.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            text = "Income",
+                            style = MaterialTheme.typography.titleLarge,
+                            fontWeight = FontWeight.SemiBold
+                        )
+                        Text(
+                            text = "${balanceState.balance?.income}",
+                            style = MaterialTheme.typography.titleLarge,
+                            fontWeight = FontWeight.SemiBold
+                        )
+                    }
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            text = "Expense",
+                            style = MaterialTheme.typography.titleLarge,
+                            fontWeight = FontWeight.SemiBold
+                        )
+                        Text(
+                            text = "${balanceState.balance?.expense}",
+                            style = MaterialTheme.typography.titleLarge,
+                            fontWeight = FontWeight.SemiBold
+                        )
+                    }
+                }
             }
         }
         Button(onClick = {
