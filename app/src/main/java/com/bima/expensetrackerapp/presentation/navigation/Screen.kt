792e0deedcd7cc59d843f829b13b10fc0d814781
Mhd Bima Taruna Cipta <bimataruna21@gmail.com>
2023-11-04 19:02:19 +0700

    add stat, setting, profile to Screen.kt

--- app/src/main/java/com/bima/expensetrackerapp/presentation/navigation/Screen.kt
+++ app/src/main/java/com/bima/expensetrackerapp/presentation/navigation/Screen.kt
@@ -1,12 +1,17 @@
 package com.bima.expensetrackerapp.presentation.navigation
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.ui.graphics.vector.ImageVector
 
 sealed class Screen(val route:String,val name:String ,val icon: ImageVector) {
     object Title: Screen("title_screen","Title", Icons.Filled.Home)
     object Login: Screen("login_screen","Login", Icons.Filled.Home)
-    object HomeScreen: Screen("home_screen","Home", Icons.Filled.Search)
+    object HomeScreen: Screen("home_screen","Home", Icons.Filled.Home)
+    object StatScreen: Screen("stat_screen","Statistic", Icons.Filled.List)
+    object SettingScreen: Screen("setting_screen","Setting", Icons.Filled.Settings)
+    object ProfileScreen: Screen("profile_screen","Profile", Icons.Filled.AccountCircle)
 }
