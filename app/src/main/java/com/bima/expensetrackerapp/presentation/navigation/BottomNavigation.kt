0613e6a9df74aa733dcc7cf8bfb6ff38f36ac5da
Mhd Bima Taruna Cipta <bimataruna21@gmail.com>
2023-11-04 18:39:28 +0700

    create BottomNavigation

---
+++ app/src/main/java/com/bima/expensetrackerapp/presentation/navigation/BottomNavigation.kt
@@ -0,0 +1,50 @@
+package com.bima.expensetrackerapp.presentation.navigation
+
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+
+@Composable
+fun BottomNavigation(
+    modifier:Modifier = Modifier,
+    navController:NavHostController,
+    screens:List<Screen>
+) {
+    val currentRoute = currentRoute(navController = navController)
+    NavigationBar(
+        modifier = Modifier.fillMaxHeight(0.12f)
+    ) {
+        screens.forEach {screen ->
+            NavigationBarItem(
+                icon = { Icon(imageVector = screen.icon, contentDescription = screen.name) },
+                label = { Text(text = screen.name) },
+                selected = currentRoute == screen.route,
+                onClick = {
+                    if (currentRoute != screen.route) {
+                        navController.navigate(screen.route) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    }
+                },
+            )
+        }
+    }
+}
+
+@Composable
+fun currentRoute(navController: NavHostController): String? {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    return navBackStackEntry?.destination?.route
+}
