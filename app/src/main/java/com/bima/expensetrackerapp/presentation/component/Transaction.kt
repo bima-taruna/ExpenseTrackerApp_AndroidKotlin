196f71fa55786f909f34ff41fd6b28aef16564e1
Mhd Bima Taruna Cipta <bimataruna21@gmail.com>
2023-11-13 23:18:22 +0700

    implement expense list in home screen

--- app/src/main/java/com/bima/expensetrackerapp/presentation/component/Transaction.kt
+++ app/src/main/java/com/bima/expensetrackerapp/presentation/component/Transaction.kt
@@ -1,11 +1,54 @@
 package com.bima.expensetrackerapp.presentation.component
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import com.bima.expensetrackerapp.viewmodel.state.ExpensesState
 
 @Composable
-fun Transaction(
-    modifier: Modifier = Modifier
+fun TransactionList(
+    state:ExpensesState,
+    modifier: Modifier = Modifier,
 ) {
-
+    val lazyColumnListState = rememberLazyListState()
+    Box {
+        LazyColumn(
+            modifier = modifier.fillMaxSize(),
+            state=lazyColumnListState
+        ) {
+            state.expenses?.size?.let {
+                items(it) { i->
+                    val expense = state.expenses[i]
+                    Row {
+                        Text(text = "${expense.name}")
+                        Text(text = "${expense.amount}")
+                    }
+                }
+            }
+        }
+        if (state.isLoading) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+        if (state.expenses?.size?.equals(0) == true) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text("list is empty")
+            }
+        }
+    }
 }
--- app/src/main/java/com/bima/expensetrackerapp/presentation/component/tabs/ExpenseTabs.kt
+++ app/src/main/java/com/bima/expensetrackerapp/presentation/component/tabs/ExpenseTabs.kt
@@ -2,8 +2,16 @@ package com.bima.expensetrackerapp.presentation.component.tabs
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.bima.expensetrackerapp.presentation.component.TransactionList
+import com.bima.expensetrackerapp.viewmodel.ExpenseViewModel
 
 @Composable
-fun ExpenseTabs() {
-    Text(text = "Expenses")
+fun ExpenseTabs(
+    expenseViewModel: ExpenseViewModel = hiltViewModel(),
+) {
+    val state by expenseViewModel.expensesState.collectAsStateWithLifecycle()
+    TransactionList(state = state)
 }
