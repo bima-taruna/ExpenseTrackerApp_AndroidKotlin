d5c654d28d190e992039263f1ba05a333fffcf02
Mhd Bima Taruna Cipta <bimataruna21@gmail.com>
2023-11-13 21:40:35 +0700

    create expense viewModel

---
+++ app/src/main/java/com/bima/expensetrackerapp/viewmodel/state/ExpenseState.kt
@@ -0,0 +1,10 @@
+package com.bima.expensetrackerapp.viewmodel.state
+
+import com.bima.expensetrackerapp.domain.model.Balance
+import com.bima.expensetrackerapp.domain.model.Expenses
+
+data class ExpensesState(
+    val isLoading:Boolean = false,
+    val expenses: List<Expenses>? = null,
+    val error:String = ""
+)
