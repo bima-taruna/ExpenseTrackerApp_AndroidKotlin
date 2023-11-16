2412bce827ad70a57c4ed63568188f24757832e6
Mhd Bima Taruna Cipta <bimataruna21@gmail.com>
2023-11-16 10:05:15 +0700

    implement category use cases : Get expense category & income

--- app/src/main/java/com/bima/expensetrackerapp/domain/use_case/category/GetExpenseCategoryUseCase.kt
+++ app/src/main/java/com/bima/expensetrackerapp/domain/use_case/category/GetExpenseCategoryUseCase.kt
@@ -1,4 +1,30 @@
 package com.bima.expensetrackerapp.domain.use_case.category
 
-class GetExpenseCategoryUseCase {
+import com.bima.expensetrackerapp.common.Resource
+import com.bima.expensetrackerapp.data.remote.CategoryDto
+import com.bima.expensetrackerapp.domain.model.Category
+import com.bima.expensetrackerapp.domain.repository.CategoryRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.withContext
+import java.io.IOException
+import javax.inject.Inject
+
+class GetExpenseCategoryUseCase @Inject constructor(
+    private val categoryRepository: CategoryRepository
+) {
+    suspend fun execute():Flow<Resource<List<CategoryDto>>> = flow {
+        try {
+            emit(Resource.Loading())
+            val result = withContext(Dispatchers.IO) {
+                categoryRepository.getExpenseCategory()
+            }
+            emit(Resource.Success(result))
+        } catch (e:Exception) {
+            emit(Resource.Error(e.localizedMessage ?: "error occured"))
+        } catch (e: IOException) {
+            emit(Resource.Error("couldn't reach server, please check your internet connection" ))
+        }
+    }
 }
--- app/src/main/java/com/bima/expensetrackerapp/domain/use_case/category/GetIncomeCategoryUseCase.kt
+++ app/src/main/java/com/bima/expensetrackerapp/domain/use_case/category/GetIncomeCategoryUseCase.kt
@@ -1,5 +1,29 @@
 package com.bima.expensetrackerapp.domain.use_case.category
 
-class GetIncomeCategoryUseCase {
+import com.bima.expensetrackerapp.common.Resource
+import com.bima.expensetrackerapp.data.remote.CategoryDto
+import com.bima.expensetrackerapp.domain.repository.CategoryRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.withContext
+import java.io.IOException
+import javax.inject.Inject
 
+class GetIncomeCategoryUseCase@Inject constructor(
+    private val categoryRepository: CategoryRepository
+) {
+    suspend fun execute(): Flow<Resource<List<CategoryDto>>> = flow {
+        try {
+            emit(Resource.Loading())
+            val result = withContext(Dispatchers.IO) {
+                categoryRepository.getIncomeCategory()
+            }
+            emit(Resource.Success(result))
+        } catch (e:Exception) {
+            emit(Resource.Error(e.localizedMessage ?: "error occured"))
+        } catch (e: IOException) {
+            emit(Resource.Error("couldn't reach server, please check your internet connection" ))
+        }
+    }
 }
