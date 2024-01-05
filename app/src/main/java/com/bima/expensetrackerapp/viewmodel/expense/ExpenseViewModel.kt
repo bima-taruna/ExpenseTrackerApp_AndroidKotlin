aa0aa55e726ecd7d38f14df185d9163edfca63b8
Mhd Bima Taruna Cipta <bimataruna21@gmail.com>
2024-01-05 14:29:29 +0700

    create delete expense method in ExpenseViewModel

--- app/src/main/java/com/bima/expensetrackerapp/viewmodel/expense/ExpenseViewModel.kt
+++ app/src/main/java/com/bima/expensetrackerapp/viewmodel/expense/ExpenseViewModel.kt
@@ -7,7 +7,9 @@ import androidx.lifecycle.viewModelScope
 import com.bima.expensetrackerapp.ExpenseTrackerApp
 import com.bima.expensetrackerapp.common.Resource
 import com.bima.expensetrackerapp.data.remote.toTransactions
+import com.bima.expensetrackerapp.domain.use_case.expense.DeleteExpenseUseCase
 import com.bima.expensetrackerapp.domain.use_case.expense.GetExpensesUseCase
+import com.bima.expensetrackerapp.viewmodel.state.expense.AddTransactionState
 import com.bima.expensetrackerapp.viewmodel.state.expense.TransactionsState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -21,9 +23,13 @@ import javax.inject.Inject
 class ExpenseViewModel @Inject constructor(
     private val context: ExpenseTrackerApp,
     private val getExpensesUseCase: GetExpensesUseCase,
+    private val deleteExpenseUseCase: DeleteExpenseUseCase
 ) : ViewModel() {
     private val _transactionsState = MutableStateFlow(TransactionsState())
     val expensesState = _transactionsState.asStateFlow()
+
+    private val _deleteExpenseState = MutableStateFlow(AddTransactionState())
+    val deleteExpenseState = _deleteExpenseState.asStateFlow();
 
     init {
         getExpenses()
@@ -52,6 +58,35 @@ class ExpenseViewModel @Inject constructor(
 
                     is Resource.Loading -> {
                         _transactionsState.value = _transactionsState.value.copy(
+                            isLoading = true
+                        )
+                    }
+                }
+            }.collect()
+        }
+    }
+
+    fun deleteExpense(id:String) {
+        viewModelScope.launch {
+            deleteExpenseUseCase.execute(id).onEach { result->
+                when (result) {
+                    is Resource.Success -> {
+                      _deleteExpenseState.value = _deleteExpenseState.value.copy(
+                          isLoading = false,
+                          transaction = true
+                      )
+                    }
+
+                    is Resource.Error -> {
+                        Toast.makeText(context, result.message, Toast.LENGTH_SHORT).show()
+                        Log.d("error", result.message.toString())
+                        _deleteExpenseState.value = _deleteExpenseState.value.copy(
+                            isLoading = false
+                        )
+                    }
+
+                    is Resource.Loading -> {
+                        _deleteExpenseState.value = _deleteExpenseState.value.copy(
                             isLoading = true
                         )
                     }
