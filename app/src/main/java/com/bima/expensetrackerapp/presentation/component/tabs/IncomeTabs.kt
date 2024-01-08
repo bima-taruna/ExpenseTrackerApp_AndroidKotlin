84eccc9ed764273857aa8e4576592cb48cd24af1
Mhd Bima Taruna Cipta <bimataruna21@gmail.com>
2024-01-08 22:26:55 +0700

    implement swipe to delete to income list

--- app/src/main/java/com/bima/expensetrackerapp/presentation/component/tabs/IncomeTabs.kt
+++ app/src/main/java/com/bima/expensetrackerapp/presentation/component/tabs/IncomeTabs.kt
@@ -9,20 +9,26 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.bima.expensetrackerapp.presentation.component.TransactionList
+import com.bima.expensetrackerapp.viewmodel.BalanceViewModel
 import com.bima.expensetrackerapp.viewmodel.income.IncomeViewModel
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun IncomeTabs(
     incomeViewModel: IncomeViewModel = hiltViewModel(),
+    balanceViewModel: BalanceViewModel = hiltViewModel(),
     navController: NavController,
 ) {
     val state by incomeViewModel.incomesState.collectAsStateWithLifecycle()
     println(state.transactions?.toString())
-    TransactionList(state = state, isIncome = true, navController = navController, swipeToDelete = {
-
-    },
+    TransactionList(
+        state = state,
+        isIncome = true,
+        navController = navController,
+        swipeToDelete = {
+            incomeViewModel.deleteIncome(it)
+        },
         updateData = {
-
+            balanceViewModel.getBalance()
         })
 }
