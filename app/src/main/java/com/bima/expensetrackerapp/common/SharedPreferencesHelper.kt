eb8648d9fe0aafc1fc4b6c5be87f82c072404beb
Mhd Bima Taruna Cipta <bimataruna21@gmail.com>
2024-02-12 14:10:57 +0700

    create SharedPreferencesHelper

---
+++ app/src/main/java/com/bima/expensetrackerapp/common/SharedPreferencesHelper.kt
@@ -0,0 +1,19 @@
+package com.bima.expensetrackerapp.common
+
+import android.content.Context
+
+class SharedPreferencesHelper(private val context: Context) {
+    companion object {
+        private const val MY_PREF_KEY = "PREF_KEY"
+    }
+
+    fun saveStringData(key:String,value:String?) {
+        val sharedPreferences = context.getSharedPreferences(MY_PREF_KEY, Context.MODE_PRIVATE)
+        sharedPreferences.edit().putString(key,value).apply()
+    }
+
+    fun getStringData(key: String):String? {
+        val sharedPreferences = context.getSharedPreferences(MY_PREF_KEY, Context.MODE_PRIVATE)
+        return sharedPreferences.getString(key,null)
+    }
+}
