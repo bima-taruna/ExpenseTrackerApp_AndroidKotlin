aa2e750733c2502003493b8e9f9950aee9b92e7c
bima-taruna <bimataruna21@gmail.com>
2024-05-06 17:28:58 +0700

    create delete category method in CategoryViewModel

--- app/src/main/java/com/bima/expensetrackerapp/viewmodel/category/CategoryViewModel.kt
+++ app/src/main/java/com/bima/expensetrackerapp/viewmodel/category/CategoryViewModel.kt
@@ -7,9 +7,11 @@ import androidx.lifecycle.viewModelScope
 import com.bima.expensetrackerapp.ExpenseTrackerApp
 import com.bima.expensetrackerapp.common.Resource
 import com.bima.expensetrackerapp.data.remote.toCategory
+import com.bima.expensetrackerapp.domain.use_case.category.DeleteCategoryUseCase
 import com.bima.expensetrackerapp.domain.use_case.category.GetExpenseCategoryUseCase
 import com.bima.expensetrackerapp.domain.use_case.category.GetIncomeCategoryUseCase
 import com.bima.expensetrackerapp.viewmodel.state.category.CategoryState
+import com.bima.expensetrackerapp.viewmodel.state.category.EventCategoryState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -24,12 +26,16 @@ class CategoryViewModel @Inject constructor(
     private val context: ExpenseTrackerApp,
     private val getExpenseCategoryUseCase: GetExpenseCategoryUseCase,
     private val getIncomeCategoryUseCase: GetIncomeCategoryUseCase,
+    private val deleteCategoryUseCase: DeleteCategoryUseCase
 ) : ViewModel() {
     private val _categoryExpenseState = MutableStateFlow(CategoryState())
     val categoryExpenseState = _categoryExpenseState.asStateFlow()
 
     private val _categoryIncomeState = MutableStateFlow(CategoryState())
     val categoryIncomeState = _categoryIncomeState.asStateFlow()
+
+    private val _deleteCategoryState = MutableStateFlow(EventCategoryState())
+    val deleteCategoryState = _deleteCategoryState.asStateFlow()
 
     fun getExpenseCategory() {
         viewModelScope.launch {
@@ -87,6 +93,33 @@ class CategoryViewModel @Inject constructor(
                         }
                     }
                 }
+            }.collect()
+        }
+    }
+
+    fun deleteCategory(id:String) {
+        viewModelScope.launch {
+            deleteCategoryUseCase.execute(id).onEach { result->
+               when(result) {
+                   is Resource.Success -> {
+                       _deleteCategoryState.update {
+                           it.copy(isLoading = false, category = result.data ?: false)
+                       }
+                       Toast.makeText(context, "category deleted", Toast.LENGTH_SHORT).show()
+                   }
+                   is Resource.Error -> {
+                       Toast.makeText(context, result.message, Toast.LENGTH_SHORT).show()
+                       Log.d("error", result.message.toString())
+                       _deleteCategoryState.update {
+                           it.copy(isLoading = false, error = result.message.toString())
+                       }
+                   }
+                   is Resource.Loading -> {
+                       _deleteCategoryState.update {
+                           it.copy(isLoading = true)
+                       }
+                   }
+               }
             }.collect()
         }
     }
