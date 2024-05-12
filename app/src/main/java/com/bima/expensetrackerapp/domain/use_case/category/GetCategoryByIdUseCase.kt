2f2b1555617e7e894ce50d3896272d129b589904
bima-taruna <bimataruna21@gmail.com>
2024-05-12 16:51:01 +0700

    map categoryDto model to category mode in GetCategoryByIdUseCase

--- app/src/main/java/com/bima/expensetrackerapp/domain/use_case/category/GetCategoryByIdUseCase.kt
+++ app/src/main/java/com/bima/expensetrackerapp/domain/use_case/category/GetCategoryByIdUseCase.kt
@@ -1,7 +1,8 @@
 package com.bima.expensetrackerapp.domain.use_case.category
 
 import com.bima.expensetrackerapp.common.Resource
-import com.bima.expensetrackerapp.data.remote.CategoryDto
+import com.bima.expensetrackerapp.data.remote.toCategory
+import com.bima.expensetrackerapp.domain.model.Category
 import com.bima.expensetrackerapp.domain.repository.CategoryRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
@@ -13,11 +14,11 @@ import javax.inject.Inject
 class GetCategoryByIdUseCase @Inject constructor(
     private val categoryRepository: CategoryRepository
 ) {
-    suspend fun execute(id:String):Flow<Resource<CategoryDto>> = flow {
+    suspend fun execute(id:String):Flow<Resource<Category>> = flow {
         try {
             emit(Resource.Loading())
             val result = withContext(Dispatchers.IO) {
-                categoryRepository.getCategoryById(id)
+                categoryRepository.getCategoryById(id).toCategory()
             }
             emit(Resource.Success(result))
         } catch (e:Exception) {
