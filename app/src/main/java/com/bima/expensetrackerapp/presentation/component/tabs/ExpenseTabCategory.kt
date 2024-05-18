e2efedaddbc826677b4f71a80d6fd9ae66bb386d
bima-taruna <bimataruna21@gmail.com>
2024-05-18 18:28:34 +0700

    <improve> [category] add loading when fetching category data by id

--- app/src/main/java/com/bima/expensetrackerapp/presentation/component/tabs/ExpenseTabCategory.kt
+++ app/src/main/java/com/bima/expensetrackerapp/presentation/component/tabs/ExpenseTabCategory.kt
@@ -1,6 +1,7 @@
 package com.bima.expensetrackerapp.presentation.component.tabs
 
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -11,6 +12,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,7 +22,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -145,6 +149,17 @@ fun ExpenseTabCategory(
                         }
                     }) {
                         Text(text = "Update")
+                    }
+                }
+                if (expenseCategoryDetailState.isLoading) {
+                    Box(
+                        modifier = modifier
+                            .fillMaxWidth()
+                            .background(Color.Black.copy(alpha = 0.4f))
+                        ,
+                        contentAlignment = Alignment.Center
+                    ) {
+                        CircularProgressIndicator()
                     }
                 }
             }
